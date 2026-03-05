77756fa22db29218b85e73fdc74805b6d3258b9f
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-03-05 21:15:48 +0530

    5th March 2026

--- Test_Cases/SwagLoginTest.py
+++ Test_Cases/SwagLoginTest.py
@@ -1,4 +1,6 @@
 import time
+
+import pytest
 from selenium import webdriver
 from Page_Classes.HomePage import SwagLabHomePage
 from Page_Classes.LoginPage import SwagLabLoginPage
@@ -11,6 +13,7 @@ class Test_SwagLabLogin:
 
     logger=LogGen.loggen()
 
+    @pytest.mark.sanity
     def test_TC1_loginWithValidData(self,initializeBrowser,request):
         driver=initializeBrowser
         login = SwagLabLoginPage(driver)
@@ -37,6 +40,7 @@ class Test_SwagLabLogin:
             assert False
         time.sleep(10)
 
+    @pytest.mark.smoke
     def test_TC2_loginWithInvalidData(self,initializeBrowser,request):
         driver=initializeBrowser
         login = SwagLabLoginPage(driver)
@@ -64,7 +68,33 @@ class Test_SwagLabLogin:
             assert False
         time.sleep(5)
 
-    # def test_TC3_veriryTotalProductsInHomePage(self, initializeBrowser, request):
+    @pytest.mark.functional
+    @pytest.mark.regression
+    def test_TC3_loginWithInvalidData(self, initializeBrowser, request):
+        driver = initializeBrowser
+        login = SwagLabLoginPage(driver)
+        login.enterUsername("standard_user")
+        self.logger.info("--username entered--")
+        time.sleep(1)
+        login.enterPassword("abcd")
+        self.logger.info("--password entered--")
+        time.sleep(1)
+        login.clickOnLoginBtn()
+        self.logger.info("--clicked on login btn--")
+        time.sleep(2)
+        actErrorMsg = login.getLoginFailedErrorMsg()
+        expErrorMsg = "Epic sadface: Username and password do not match any user in this service"
 
+        print("act Error: ", actErrorMsg)
+        print("Exp Error: ", expErrorMsg)
+
+        if actErrorMsg == expErrorMsg:
+            self.logger.info("--act & exp login failed error msg matched--")
+            assert True
+        else:
+            ScreenshotUtility.captureSS(driver, request.node.name)
+            self.logger.info("--act & exp login failed error msg matched--")
+            assert False
+        time.sleep(5)
 
 
